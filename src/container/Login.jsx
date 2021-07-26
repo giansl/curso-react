56038c509fc80b50cff8d44908fc73ab0b312cdb
Gian Solis Linares <gian.solis@lac.int>
2021-07-26 16:28:08 -0500

    login: creando un componente de login

--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
+import { loginRequest } from '../actions';
 import '../assets/styles/components/Login.scss'
 
-const Login = () => (
+const Login = props => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+  
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.loginRequest(form);
+    props.history.push('/');
+  }
+  return (
     <section className="login">
     <section className="login__container">
       <h2>Inicia sesión</h2>
-      <form className="login__container--form">
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
+      <form className="login__container--form" onSubmit={handleSubmit}>
+        <input
+          name="email"
+          className="input"
+          type="text"
+          placeholder="Correo"
+          onChange={handleInput} />
+        <input
+          name="password"
+          className="input"
+          type="password"
+          placeholder="Contraseña" />
         <button className="button">Iniciar sesión</button>
         <div className="login__container--remember-me">
           <label>
@@ -31,5 +59,10 @@ const Login = () => (
     </section>
   </section>
 );
+}
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+}
+
+export default connect(null, mapDispatchToProps)(Login)
