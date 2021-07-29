a5f20889096422b090c3d4e0ca1995b6cbf02b54
giansl <giansolis93@gmail.com>
2021-07-29 14:33:14 -0500

    agregando basename

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
 import Home from '../container/Home';
 import Login from '../container/Login';
 import Register from '../container/Register';
@@ -8,7 +8,7 @@ import Layout from '../components/Layout';
 import Player from '../container/Player';
 
 const App = () => (
-    <BrowserRouter>
+    <BrowserRouter basename="https://giansl.github.io/curso-react/">
         <Layout>
             <Switch>
                 <Route exact path="/" component={Home} />
