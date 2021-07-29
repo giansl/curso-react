ca6fb509b6fab1541b388598491c37a827e52e0d
giansl <giansolis93@gmail.com>
2021-07-29 14:43:08 -0500

    redirecr

--- src/routes/App.js
+++ src/routes/App.js
@@ -6,16 +6,16 @@ import Register from '../container/Register';
 import NotFound from '../container/NotFound';
 import Layout from '../components/Layout';
 import Player from '../container/Player';
-
+// <Route component={NotFound} />
 const App = () => (
     <BrowserRouter >
         <Layout>
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route component={Home} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/player/:id" component={Player} />
-                <Route component={NotFound} />
+                
             </Switch>
         </Layout>
     </BrowserRouter>
